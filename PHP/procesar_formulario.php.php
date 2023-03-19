d20de677c3b72ae997f9f75c4d206b3499e22876
mauroociappinaph <ciappinamauroo@gmail.com>
2023-03-19 18:07:25 -0300

    php form

--- PHP/procesar_formulario.php.php
+++ PHP/procesar_formulario.php.php
@@ -1,19 +1,23 @@
 <?php
-// Obtener los datos del formulario
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+// Verificar si se ha enviado el formulario
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-// Validar los datos
-if (empty($nombre) || empty($email) || empty($mensaje)) {
-    // Si falta algún dato requerido, mostrar un error
-    echo "Por favor, complete todos los campos.";
-} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    // Si el correo electrónico no es válido, mostrar un error
-    echo "Por favor, ingrese un correo electrónico válido.";
-} else {
-    // Si los datos son válidos, procesar el formulario
-    // ... Aquí puede agregar su código para procesar el formulario ...
-    echo "Gracias por enviar el formulario.";
+    // Obtener los datos del formulario
+    $nombre = trim($_POST['nombre']);
+    $email = trim($_POST['email']);
+    $mensaje = trim($_POST['mensaje']);
+
+    // Validar los datos
+    if (empty($nombre) || empty($email) || empty($mensaje)) {
+        // Si falta algún dato requerido, mostrar un error
+        echo "Por favor, complete todos los campos.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Si el correo electrónico no es válido, mostrar un error
+        echo "Por favor, ingrese un correo electrónico válido.";
+    } else {
+        // Si los datos son válidos, procesar el formulario
+        // ... Aquí puede agregar su código para procesar el formulario ...
+        echo "Gracias por enviar el formulario.";
+    }
 }
-
+?>
